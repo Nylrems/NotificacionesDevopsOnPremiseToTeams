d1bb501fd99d977ce42212a99105a21e83bdb00b
smerlyn.garcia <smerlyn.garcia@idoppril.gob.do>
2024-08-30 08:13:34 -0400

    Modificando la tarjeta e información de buildComplete y agregando tarjeta e información de pullRequest

---
+++ src/functions/pullRequest.js
@@ -0,0 +1,122 @@
+const axios = require('axios');
+const util = require('util');
+const { app } = require('@azure/functions');
+
+app.http('pullRequestCreated', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log('Received request to transform payload.');
+
+        const teamsWebhookUrl = process.env.TEAMS_WEBHOOK_URL;
+        if (!teamsWebhookUrl) {
+            context.log('Teams Webhook URL is not defined.');
+            return {
+                status: 500,
+                body: "Teams Webhook URL is not defined"
+            };
+        }
+
+        try {
+            const reqBody = await request.json();
+            context.log('Received payload:', util.inspect(reqBody, { depth: null }));
+
+            const title = reqBody.resource.title;
+            const repositoryName = reqBody.resource.repository.name;
+            const sourceRefName = reqBody.resource.sourceRefName;
+            const targetRefName = reqBody.resource.targetRefName;
+            const mergeStatus = reqBody.resource.mergeStatus;
+            const pullRequestId = reqBody.resource.pullRequestId;
+            const description = reqBody.resource.description;
+            const creator = reqBody.resource.reviewers[0].displayName || 'Seguro fuíste tú';
+            const pullRequestUrl = reqBody.resource._links.web.href;
+
+
+            const adaptiveCardPayload = {
+                "type": "AdaptiveCard",
+                "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                "version": "1.3",
+                "body": [
+                    {
+                        "type": "TextBlock",
+                        "size": "Large",
+                        "weight": "Default",
+                        "text": "Pull Request Creado",
+                        "style": "heading",
+                        "fontType": "Default"
+                    },
+                    {
+                        "type": "TextBlock",
+                        "text": `Título: ${title}`,
+                        "wrap": true
+                    },
+                    {
+                        "type": "FactSet",
+                        "facts": [
+                            {
+                                "title": "Repositorio",
+                                "value": repositoryName
+                            },
+                            {
+                                "title": "Rama:",
+                                "value": sourceRefName
+                            },
+                            {
+                                "title": "Rama Target:",
+                                "value": targetRefName
+                            },
+                            {
+                                "title": "Estado:",
+                                "value": mergeStatus
+                            },
+                            {
+                                "title": "Pull Request ID:",
+                                "value": pullRequestId
+                            },
+                            {
+                                "title": "Descripción:",
+                                "value": description
+                            },
+                            {
+                                "title": "Creado por: ",
+                                "value": creator
+                            }
+                        ]
+                    }
+                ],
+                "actions": [
+                    {
+                        "type": "Action.OpenUrl",
+                        "title": "Ver Pull Request",
+                        "url": pullRequestUrl
+                    }
+                ]
+            };
+
+            context.log('Transformed Payload:', util.inspect(adaptiveCardPayload, { depth: null }));
+
+            const response = await axios.post(teamsWebhookUrl, adaptiveCardPayload);
+
+            if (response.status === 200 || response.status === 202) {
+                context.log('Payload sent successfully');
+                return {
+                    status: 200,
+                    body: "Payload sent successfully"
+                };
+            } else {
+                context.log(`Error sending payload to Teams: ${response.statusText}`);
+                return {
+                    status: 500,
+                    body: `Error sending payload to Teams: ${response.statusText}`
+                };
+            }
+        } catch (err) {
+            context.log(`Invalid request body or error sending to Teams: ${util.inspect(err)}`);
+            return {
+                status: 400,
+                body: "Invalid request body or error sending to Teams"
+            };
+        }
+    }
+}
+);
